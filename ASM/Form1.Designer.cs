2fd84d68e96320c493eacf7423854869e49278ca
phuchlh <hongphucthd@gmail.com>
2021-07-09 12:00:26 +0700

    My first project with C# - Assignment

---
+++ ASM/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace ASM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataSinhVien = new System.Windows.Forms.DataGridView();
+            this.tENDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mASVDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nAMDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mAKHDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sVIENBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.qLSVienDataSet = new ASM.QLSVienDataSet();
+            this.sVIENTableAdapter = new ASM.QLSVienDataSetTableAdapters.SVIENTableAdapter();
+            this.qLSVienDataSet1 = new ASM.QLSVienDataSet1();
+            this.kHOABindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.kHOATableAdapter = new ASM.QLSVienDataSet1TableAdapters.KHOATableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.txtMSSV = new System.Windows.Forms.TextBox();
+            this.txtNam = new System.Windows.Forms.TextBox();
+            this.txtMaKH = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCanel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSinhVien)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sVIENBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLSVienDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLSVienDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kHOABindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataSinhVien
+            // 
+            this.dataSinhVien.AutoGenerateColumns = false;
+            this.dataSinhVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataSinhVien.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tENDataGridViewTextBoxColumn,
+            this.mASVDataGridViewTextBoxColumn,
+            this.nAMDataGridViewTextBoxColumn,
+            this.mAKHDataGridViewTextBoxColumn});
+            this.dataSinhVien.DataSource = this.sVIENBindingSource;
+            this.dataSinhVien.Location = new System.Drawing.Point(12, 12);
+            this.dataSinhVien.Name = "dataSinhVien";
+            this.dataSinhVien.Size = new System.Drawing.Size(410, 306);
+            this.dataSinhVien.TabIndex = 0;
+            this.dataSinhVien.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataSinhVien_CellContentClick);
+            // 
+            // tENDataGridViewTextBoxColumn
+            // 
+            this.tENDataGridViewTextBoxColumn.DataPropertyName = "TEN";
+            this.tENDataGridViewTextBoxColumn.HeaderText = "TEN";
+            this.tENDataGridViewTextBoxColumn.Name = "tENDataGridViewTextBoxColumn";
+            // 
+            // mASVDataGridViewTextBoxColumn
+            // 
+            this.mASVDataGridViewTextBoxColumn.DataPropertyName = "MASV";
+            this.mASVDataGridViewTextBoxColumn.HeaderText = "MASV";
+            this.mASVDataGridViewTextBoxColumn.Name = "mASVDataGridViewTextBoxColumn";
+            // 
+            // nAMDataGridViewTextBoxColumn
+            // 
+            this.nAMDataGridViewTextBoxColumn.DataPropertyName = "NAM";
+            this.nAMDataGridViewTextBoxColumn.HeaderText = "NAM";
+            this.nAMDataGridViewTextBoxColumn.Name = "nAMDataGridViewTextBoxColumn";
+            // 
+            // mAKHDataGridViewTextBoxColumn
+            // 
+            this.mAKHDataGridViewTextBoxColumn.DataPropertyName = "MAKH";
+            this.mAKHDataGridViewTextBoxColumn.HeaderText = "MAKH";
+            this.mAKHDataGridViewTextBoxColumn.Name = "mAKHDataGridViewTextBoxColumn";
+            // 
+            // sVIENBindingSource
+            // 
+            this.sVIENBindingSource.DataMember = "SVIEN";
+            this.sVIENBindingSource.DataSource = this.qLSVienDataSet;
+            // 
+            // qLSVienDataSet
+            // 
+            this.qLSVienDataSet.DataSetName = "QLSVienDataSet";
+            this.qLSVienDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sVIENTableAdapter
+            // 
+            this.sVIENTableAdapter.ClearBeforeFill = true;
+            // 
+            // qLSVienDataSet1
+            // 
+            this.qLSVienDataSet1.DataSetName = "QLSVienDataSet1";
+            this.qLSVienDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // kHOABindingSource
+            // 
+            this.kHOABindingSource.DataMember = "KHOA";
+            this.kHOABindingSource.DataSource = this.qLSVienDataSet1;
+            // 
+            // kHOATableAdapter
+            // 
+            this.kHOATableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(452, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ten";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(452, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MSSV";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(452, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nam";
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(505, 25);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(207, 20);
+            this.txtTen.TabIndex = 9;
+            // 
+            // txtMSSV
+            // 
+            this.txtMSSV.Location = new System.Drawing.Point(505, 83);
+            this.txtMSSV.Name = "txtMSSV";
+            this.txtMSSV.Size = new System.Drawing.Size(207, 20);
+            this.txtMSSV.TabIndex = 10;
+            // 
+            // txtNam
+            // 
+            this.txtNam.Location = new System.Drawing.Point(505, 142);
+            this.txtNam.Name = "txtNam";
+            this.txtNam.Size = new System.Drawing.Size(207, 20);
+            this.txtNam.TabIndex = 11;
+            // 
+            // txtMaKH
+            // 
+            this.txtMaKH.Location = new System.Drawing.Point(505, 206);
+            this.txtMaKH.Name = "txtMaKH";
+            this.txtMaKH.Size = new System.Drawing.Size(207, 20);
+            this.txtMaKH.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(452, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "MaKH";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(455, 261);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 15;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(559, 261);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 16;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(663, 261);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(455, 295);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 18;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCanel
+            // 
+            this.btnCanel.Location = new System.Drawing.Point(559, 295);
+            this.btnCanel.Name = "btnCanel";
+            this.btnCanel.Size = new System.Drawing.Size(75, 23);
+            this.btnCanel.TabIndex = 19;
+            this.btnCanel.Text = "Cancel";
+            this.btnCanel.UseVisualStyleBackColor = true;
+            this.btnCanel.Click += new System.EventHandler(this.btnCanel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(663, 295);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 20;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 339);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCanel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMaKH);
+            this.Controls.Add(this.txtNam);
+            this.Controls.Add(this.txtMSSV);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataSinhVien);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSinhVien)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sVIENBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLSVienDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLSVienDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kHOABindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataSinhVien;
+        private QLSVienDataSet qLSVienDataSet;
+        private System.Windows.Forms.BindingSource sVIENBindingSource;
+        private QLSVienDataSetTableAdapters.SVIENTableAdapter sVIENTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tENDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mASVDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nAMDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mAKHDataGridViewTextBoxColumn;
+        private QLSVienDataSet1 qLSVienDataSet1;
+        private System.Windows.Forms.BindingSource kHOABindingSource;
+        private QLSVienDataSet1TableAdapters.KHOATableAdapter kHOATableAdapter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.TextBox txtMSSV;
+        private System.Windows.Forms.TextBox txtNam;
+        private System.Windows.Forms.TextBox txtMaKH;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCanel;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ ASM/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace ASM
+{
+    public partial class Form1 : Form
+    {
+        SqlDataAdapter dAdapt;
+        public static DataSet myds = new DataSet("QLSVien");
+        public static SqlConnection cnStr =
+                new SqlConnection(@"server=localhost\HPHUC;User ID =sa;Pwd=HongPhuc228;database=QLSVien");
+        public Form1()
+        {
+            InitializeComponent();
+            initDataSet();
+            loadData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'qLSVienDataSet1.KHOA' table. You can move, or remove it, as needed.
+            this.kHOATableAdapter.Fill(this.qLSVienDataSet1.KHOA);
+            // TODO: This line of code loads data into the 'qLSVienDataSet.SVIEN' table. You can move, or remove it, as needed.
+            this.sVIENTableAdapter.Fill(this.qLSVienDataSet.SVIEN);
+            dataSinhVien.ReadOnly = true;
+        }
+
+        public void initDataSet()
+        {
+            dAdapt = new SqlDataAdapter("Select * from SVIEN", cnStr);
+            SqlCommandBuilder svBuilder = new SqlCommandBuilder(dAdapt);
+        }
+
+        public void loadData()
+        {
+            try
+            {
+                dAdapt.Fill(myds, "SVIEN");
+                dataSinhVien.DataSource = myds.Tables["SVIEN"];
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Can not load data " + e.Message);
+                dataSinhVien.DataSource = null;
+            }
+        }
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            cancelClick();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnCanel_Click(object sender, EventArgs e)
+        {
+            if (btnCanel != null)
+            {
+                cancelClick();
+            }
+        }
+
+        public void cancelClick()
+        {
+            txtTen.Text = string.Empty;
+            txtMSSV.Text = string.Empty;
+            txtMaKH.Text = string.Empty;
+            txtNam.Text = string.Empty;
+        }
+
+        private void dataSinhVien_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = dataSinhVien.CurrentRow.Index;
+            txtMSSV.ReadOnly = true;
+
+            this.txtTen.Text = dataSinhVien.Rows[index].Cells[0].Value.ToString();
+            this.txtMSSV.Text = dataSinhVien.Rows[index].Cells[1].Value.ToString();
+            this.txtNam.Text = dataSinhVien.Rows[index].Cells[2].Value.ToString();
+            this.txtMaKH.Text = dataSinhVien.Rows[index].Cells[3].Value.ToString();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            addNew();
+        }
+
+        public void addNew()
+        {
+            bool checkMSSV = checkInputMSSV(Int32.Parse(this.txtMSSV.Text));
+            bool checkNam = checkInputNam(Int32.Parse(this.txtNam.Text));
+            bool checkMaKH = checkInputMAKH(this.txtMaKH.Text);
+            
+            if (!checkMSSV || !checkNam || !checkMaKH)
+            {
+                MessageBox.Show("Err ");
+            }
+            else
+            {
+                DataRow newStudent = (DataRow)myds.Tables["SVIEN"].NewRow();
+                newStudent["TEN"] = this.txtTen.Text;
+                newStudent["MASV"] = Int32.Parse(this.txtMSSV.Text);
+                newStudent["NAM"] = Int32.Parse(this.txtNam.Text);
+                newStudent["MAKH"] = this.txtMaKH.Text;
+                myds.Tables["SVIEN"].Rows.Add(newStudent);
+                cancelClick();
+            }
+            try
+            {
+                dAdapt.Update(myds.Tables["SVIEN"]);
+                //cancelClick();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Have problem with add new student " + e.Message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            deleteData();
+            cancelClick();
+        }
+
+        public void deleteData()
+        {
+            try
+            {
+                if (txtMSSV.Text != null)
+                {
+                    int delRow = dataSinhVien.CurrentCell.RowIndex;
+                    myds.Tables["SVIEN"].Rows[delRow].Delete();
+                    dAdapt.Update(myds.Tables["SVIEN"]);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Have error with delete student " + e.Message);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            addNew();
+        }
+        public bool checkInputMSSV(int mssv)
+        {
+            if (mssv == 0) return false;
+            else return true;
+        }
+
+        public bool checkInputNam (int nam)
+        {
+            if(nam == 0 || nam < 1 || nam > 4) return false;
+            else return true;
+        }
+
+        public bool checkInputMAKH (string ma)
+        {
+            if (Regex.IsMatch(ma, "SE|IA")) return true;
+            else return false;
+        }
+
+        
+    }
+}
